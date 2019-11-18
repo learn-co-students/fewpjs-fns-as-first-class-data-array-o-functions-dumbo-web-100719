51c8d3c8f95324c73bb8d200b14c5fc5c13bc0a3
robin-raq <robin.raq@gmail.com>
2019-11-18 14:34:17 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+function wakeDog(name, breed){
+    return `Wake ${name} the ${breed}`
+}
+function leashDog(name, breed){
+    return `Leash ${name} the ${breed}`
+}
+function walkToPark(name, breed){
+    return `Walk to the park with ${name} the ${breed}`
+}
+function throwFrisbee(name, breed){
+    return `Throw the frisbee for ${name} the ${breed}`
+}
+
+function walkHome(name, breed){
+    
+    return `Walk home with ${name} the ${breed}`
+}
+
+function unleashDog(name, breed){
+    return `Unleash ${name} the ${breed}`
+}
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+function exerciseDog(name, breed){
+    let results = []
+    for (let i=0; i< routine.length; i++){
+        results.push(routine[i](name, breed))
+    }
+    return results
+}
+
+// function exerciseDog(name, breed){
+    
+//     // This could be run inside the forEach as runFn instead
+//     // function runFn(fn){
+//     //     return fn(name, breed)
+//     // }
+
+//     return routine.forEach(function(fn){ fn(name, breed)})
+// }
